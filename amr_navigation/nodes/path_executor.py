a56fdf96898e00f350086ca59ca8cc07fdfdab88
Roberto Mendieta <roberto.mendieta@smail.inf.h-brs.de>
2016-05-26 12:11:46 +0200

    path_executor functionality

--- amr_navigation/nodes/path_executor.py
+++ amr_navigation/nodes/path_executor.py
@@ -13,13 +13,42 @@ from amr_msgs.msg import MoveToAction, MoveToGoal, ExecutePathAction, \
                          ExecutePathFeedback, ExecutePathResult
 
 
-class PathExecutor:
+class PathExecutor: 
+    # create messages that are used to publish feedback/result
+    _feedback = ExecutePathFeedback()
+    _result   = ExecutePathResult()
 
-    def __init__(self):
-        pass
+    def __init__(self): 
+        self._as = SimpleActionServer('/path_executor/execute_path', 
+            ExecutePathAction, execute_cb=self.execute_cb, 
+            auto_start = False)
+        self._as.start()
+        rospy.loginfo("path_executor created\n")
+        
+        self.move_client = SimpleActionClient('/motion_controller/move_to/', MoveToAction)
+        self.move_client.wait_for_server()
+        #pass
 
     def execute_cb(self, goal):
-        pass
+        # helper variables
+        rospy.loginfo("Goal received\n")
+        r = rospy.Rate(1)
+        
+        #Can read goal from .action msgs
+        if goal.skip_unreachable:
+            rospy.loginfo("Skip unreachable = True\n")
+  
+        # publish the feedback
+        self._as.publish_feedback(self._feedback)
+        
+        
+        #Publish to Move_to_server
+        goal2 = MoveToAction
+        goal2.target_pose = goal.path.poses[0]
+        self.move_client.send_goal(goal2)        
+        
+        r.sleep()
+        #pass
 
     def move_to_done_cb(self, state, result):
         pass
